46817ac0122dc85dbc40f52ac4dbc677c1c2366c
deedeedextor <zavinagi_15@abv.bg>
2020-02-26 16:10:38 +0200

    Refactor GlobalConstants file

--- PatniListi.Common/GlobalConstants.cs
+++ PatniListi.Common/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string SystemName = "PatniListi";
+        public const string SystemName = "Пътни листи";
 
         public const string AdministratorRoleName = "Admin";
 
