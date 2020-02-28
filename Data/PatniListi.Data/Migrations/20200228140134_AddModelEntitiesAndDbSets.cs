7272d6333851df97a8e505129faead185cb15371
deedeedextor <zavinagi_15@abv.bg>
2020-02-28 16:12:38 +0200

    Add Migration for application models and context

---
+++ Data/PatniListi.Data/Migrations/20200228140134_AddModelEntitiesAndDbSets.cs
@@ -0,0 +1,336 @@
+﻿namespace PatniListi.Data.Migrations
+{
+    using System;
+
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddModelEntitiesAndDbSets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Settings",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "AspNetRoles",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(maxLength: 20, nullable: false),
+                    LicensePlate = table.Column<string>(maxLength: 10, nullable: false),
+                    StartKilometers = table.Column<double>(nullable: false),
+                    AverageConsumption = table.Column<int>(nullable: false),
+                    TankCapacity = table.Column<double>(nullable: false),
+                    InitialFuel = table.Column<double>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(maxLength: 20, nullable: false),
+                    Bulstat = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Routes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    StartPoint = table.Column<string>(nullable: false),
+                    EndPoint = table.Column<string>(nullable: false),
+                    Distance = table.Column<double>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarUsers",
+                columns: table => new
+                {
+                    CarId = table.Column<string>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarUsers", x => new { x.CarId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_CarUsers_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CarUsers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    Number = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Location = table.Column<string>(maxLength: 20, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Quantity = table.Column<double>(nullable: false),
+                    VAT = table.Column<double>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    CarId = table.Column<string>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Invoices_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransportWorkTickets",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    CarId = table.Column<string>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransportWorkTickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TransportWorkTickets_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TransportWorkTickets_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RouteTransportWorkTickets",
+                columns: table => new
+                {
+                    RouteId = table.Column<string>(nullable: false),
+                    TransportWorkTicketId = table.Column<string>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RouteTransportWorkTickets", x => new { x.RouteId, x.TransportWorkTicketId });
+                    table.ForeignKey(
+                        name: "FK_RouteTransportWorkTickets_Routes_RouteId",
+                        column: x => x.RouteId,
+                        principalTable: "Routes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RouteTransportWorkTickets_TransportWorkTickets_TransportWorkTicketId",
+                        column: x => x.TransportWorkTicketId,
+                        principalTable: "TransportWorkTickets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_IsDeleted",
+                table: "Cars",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarUsers_UserId",
+                table: "CarUsers",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_IsDeleted",
+                table: "Companies",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_CarId",
+                table: "Invoices",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_IsDeleted",
+                table: "Invoices",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_IsDeleted",
+                table: "Routes",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RouteTransportWorkTickets_TransportWorkTicketId",
+                table: "RouteTransportWorkTickets",
+                column: "TransportWorkTicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransportWorkTickets_CarId",
+                table: "TransportWorkTickets",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransportWorkTickets_IsDeleted",
+                table: "TransportWorkTickets",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransportWorkTickets_UserId",
+                table: "TransportWorkTickets",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "CarUsers");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "RouteTransportWorkTickets");
+
+            migrationBuilder.DropTable(
+                name: "Routes");
+
+            migrationBuilder.DropTable(
+                name: "TransportWorkTickets");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "AspNetRoles");
+        }
+    }
+}
