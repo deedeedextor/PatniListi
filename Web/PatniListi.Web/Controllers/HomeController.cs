b22588e9ff36680bad1716768e400ae60b1c9e03
deedeedextor <zavinagi_15@abv.bg>
2020-03-11 18:15:08 +0200

    Add IndexLoggedIn html for HomeController

--- Web/PatniListi.Web/Controllers/HomeController.cs
+++ Web/PatniListi.Web/Controllers/HomeController.cs
@@ -1,15 +1,47 @@
 ﻿namespace PatniListi.Web.Controllers
 {
     using System.Diagnostics;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
 
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using PatniListi.Data.Models;
+    using PatniListi.Services.Data;
     using PatniListi.Web.ViewModels;
+    using PatniListi.Web.ViewModels.Models.Users;
 
     public class HomeController : BaseController
     {
+        private readonly IUsersService usersService;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public HomeController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        [HttpGet]
+        [Route("/")]
         public IActionResult Index()
         {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return this.RedirectToAction(nameof(this.IndexLoggedIn));
+            }
+
             return this.View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> IndexLoggedIn()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var viewModel = await this.usersService.GetByIdAsync<ApplicationUserHomeViewModel>(userId);
+            return this.View(viewModel);
         }
 
         public IActionResult Contact()
