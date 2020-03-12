a26679b26a2c4a283aceb3c7154194bf3186e374
deedeedextor <zavinagi_15@abv.bg>
2020-03-12 23:47:32 +0200

    Change: *Add Migration *Fix references *Add validation script file

---
+++ Data/PatniListi.Data/Migrations/20200312202351_ChangeDataAnnotationAndPropertities.cs
@@ -0,0 +1,59 @@
+﻿namespace PatniListi.Data.Migrations
+{
+    using System;
+
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class ChangeDataAnnotationAndPropertities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Invoices",
+                nullable: false,
+                defaultValue: string.Empty);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FuelType",
+                table: "Invoices",
+                nullable: false,
+                defaultValue: string.Empty);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VatNumber",
+                table: "Companies",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastLoggingDate",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "FuelType",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "VatNumber",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastLoggingDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
