a4da2837760e2fc3764bd9e28bd519181e512fd6
deedeedextor <zavinagi_15@abv.bg>
2020-03-13 20:01:34 +0200

    Introduce: *PaginatedList *Contacts controller with views and view models

---
+++ Web/PatniListi.Web.ViewModels/Models/Contacts/ContactFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace PatniListi.Web.ViewModels.Models.Contacts
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactFormViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашите имена")]
+        [Display(Name = "Вашите имена")]
+        public string FullName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашият имейл адрес")]
+        [Display(Name = "Вашият имейл адрес")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете заглавие на съобщението")]
+        [StringLength(50, ErrorMessage = "Заглавието трябва да е поне {2} и не повече от {1} символа.", MinimumLength = 5)]
+        [Display(Name = "Заглавие на съобщението")]
+        public string Title { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете съдържание на съобщението")]
+        [StringLength(10000, ErrorMessage = "Съобщението трябва да е поне {2} символа.", MinimumLength = 20)]
+        [Display(Name = "Съдържание на съобщението")]
+        public string Content { get; set; }
+    }
+}
---
+++ Web/PatniListi.Web/Controllers/ContactsController.cs
@@ -0,0 +1,62 @@
+﻿namespace PatniListi.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using PatniListi.Common;
+    using PatniListi.Data.Common.Repositories;
+    using PatniListi.Data.Models;
+    using PatniListi.Services.Messaging;
+    using PatniListi.Web.ViewModels.Models.Contacts;
+
+    public class ContactsController : BaseController
+    {
+        private readonly IRepository<ContactFormEntry> contactsRepository;
+        private readonly IEmailSender emailSender;
+
+        public ContactsController(IRepository<ContactFormEntry> contactsRepository, IEmailSender emailSender)
+        {
+            this.contactsRepository = contactsRepository;
+            this.emailSender = emailSender;
+        }
+
+        public IActionResult Contact()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(ContactFormViewModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            var viewModel = new ContactFormEntry
+            {
+                FullName = input.FullName,
+                Email = input.Email,
+                Title = input.Title,
+                Content = input.Content,
+                Ip = ip,
+            };
+
+            await this.contactsRepository.AddAsync(viewModel);
+            await this.contactsRepository.SaveChangesAsync();
+
+            await this.emailSender.SendEmailAsync(
+                input.Email, input.FullName, GlobalConstants.SystemEmail, input.Title, input.Content);
+
+            return this.RedirectToAction("ThankYou");
+        }
+
+        public IActionResult ThankYou()
+        {
+            return this.View();
+        }
+    }
+}
